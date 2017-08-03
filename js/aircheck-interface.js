2f31f4f12583ee154434c114c0286ba3b99ab411
Mick Sexton <lacrookedbeat@hotmail.com>
2017-08-03 10:29:02 -0700

    add aircheck-interface along with logic to rate the air quality

--- js/aircheck-interface.js
+++ js/aircheck-interface.js
@@ -1,1 +1,43 @@
 var AirCheck = require('./../js/aircheck.js').airCheckModule;
+
+var getStats = function(response) {
+  $('#output-city').text(response.data.city);
+  $('#output-state').text(response.data.state);
+  $('#aqius').text(response.pollution.aqius);
+
+  var aqius = response.pollution.aqius;
+  var rating = getRating(aqius);
+  $('#rating').text(rating);
+
+};
+
+var getRating = function(aqius) {
+  if (aqius < 51) {
+    return "Good";
+  } else if (aqius < 101) {
+    return "Moderate";
+  } else if (aqius < 151) {
+    return "Unhealthy for Sensitive Groups";
+  } else if (aqius < 201) {
+    return "Unhealthy";
+  } else if (aqius < 301) {
+    return "Very Unhealthy";
+  } else if {
+    return "Hazardous";
+  }
+};
+
+$(document).ready(function() {
+  var newAirCheck = new AirCheck();
+
+  $('#location-form').submit(function(event) {
+    event.preventDefault();
+    var city = $('#input-city').val();
+    var state = $('#input-state').val();
+    var country = $('#input-country').val();
+    $('#input-city').empty();
+    $('#input-state').empty();
+    // $('#input-country').empty();
+    newAirCheck.getAirQuality(city, state, country, getStats);
+  });
+});
